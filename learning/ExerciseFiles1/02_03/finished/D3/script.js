ac04f62a17aa25d72403d3c9da751efc6c8ef65f
t38dit <T38@T38-MacBook-Pro.local>
2017-02-12 18:16:01 -0700

    2

---
+++ learning/ExerciseFiles1/02_03/finished/D3/script.js
@@ -0,0 +1,11 @@
+d3.selectAll('.item:nth-child(3)')
+ 
+  .style({
+    'background': '#268BD2',
+    'padding': '10px',
+    'margin' : '35px',
+    'color' : '#EEE8D5'
+  })
+  .append('div')
+  .html('<strong>selected</strong>')
+  
